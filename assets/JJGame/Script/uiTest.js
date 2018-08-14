1f84525b581fd37dcb49a28dfce31f0a474e4dd8
zr <814693395@qq.com>
2018-08-14 20:53:34 +0800

    up

--- assets/JJGame/Script/uiTest.js
+++ assets/JJGame/Script/uiTest.js
@@ -6,9 +6,15 @@ cc.Class({
     onLoad() {
         this._super();
         this.nodeDict["start"].on("click", this.startGame, this);
+        this.nodeDict["close"].on("click", this.onCloseUI, this);
     },
 
     startGame() {
         console.log('start game ')
-    }
+    },
+
+    onCloseUI: function () {
+        window.UIMgr.closeUI("uiTest");
+        this.node.destroy();
+    },
 });
